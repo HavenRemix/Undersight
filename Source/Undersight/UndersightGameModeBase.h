2c8f3239e5eaf0f732d03c7a7bec07242177aed6
Haven Remix <30853125+HavenRemix@users.noreply.github.com>
2019-09-17 18:27:20 -0700

    Source Files

---
+++ Source/Undersight/UndersightGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UndersightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNDERSIGHT_API AUndersightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
